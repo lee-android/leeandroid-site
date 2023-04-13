2f3386505dea1aa3ded1931380c8aa9bd975f4a6
lee-android <jatyelsom@gmail.com>
2023-04-13 18:18:14 -0500

    update main page to static.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
-$(function(){
-  for(i=0;i<4;i++){
-    $('.buzz_wrapper .text span').eq(0).clone().prependTo('.buzz_wrapper .text');
+var play=false; // switch on\off
+var ii=null; // interval ID
+var colors=['rgba(0,0,0,0.35)'
+            ,'rgba(255,255,255,.35)'
+            ,'rgba(0,0,0,0.35)'
+            ,'rgba(0,0,0,0.35)'
+           ]; // pixel colors
+
+//  make a new renderer 
+function getRender(pdx,pdy,pcolors) {
+  var cvs = document.getElementById("cvs");
+  var ctx = cvs.getContext('2d');
+  var wt = cvs.width;
+  var ht = cvs.height;
+  var dx = pdx;
+  var dy = pdy;
+  var colors = pcolors;
+  return function() {
+    for(var x=0;x<wt;x+=dx)
+      for(var y=0;y<ht;y+=dy) {
+        var idx = Math.floor(Math.random()*colors.length);
+        ctx.fillStyle = colors[idx];
+        ctx.fillRect(x, y, dx, dy);
+      }
   }
-  for(i=0;i<10;i++){
-    $('.buzz_wrapper .scanline').eq(0).clone().appendTo('.buzz_wrapper');
-  }
-});
+};
+
+// one to go, please
+var render = getRender(1,2,colors);
+
+// define switcher
+function doSwitch(e) {
+  play=!play;
+  if (play)
+    ii = setInterval(render,0);
+  else
+    clearInterval(ii);
+};
+// switch ON
+doSwitch();
+// link switcher to keyboard
+document.body.addEventListener('keydown',doSwitch);
